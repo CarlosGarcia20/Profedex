5e972e63e11b3becb84de905504afaab1ad69dbf
CarlosGarcia20 <cargarcia085@gmail.com>
2025-11-28 18:04:57 -0700

    Se creo de nuevo el archivo, si tenia uso jaja

---
+++ src/components/header/HeaderGeneral.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HeaderGeneral = ({ titulo }) => {
+    return (
+        <header className="bg-yellow-300 h-22 flex items-center pl-6" style={{ boxShadow: '0 5px 10px -3px rgba(0,0,0,0.5)' }}>
+            <h1
+                className="text-5xl font-bold text-white"
+                style={{ WebkitTextStroke: '1px black' }}
+            >
+                {titulo}
+            </h1>
+        </header>
+    );
+};
+
+export default HeaderGeneral;
