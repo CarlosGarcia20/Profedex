8ca888d2122b12f20206562854ab60856022bf7e
CarlosGarcia20 <cargarcia085@gmail.com>
2025-11-28 17:53:21 -0700

    Corrección de la variable baseURL en la configuración de AXIOS para utilizar VITE_URL_API  y añadir las cookies automaticamente para enviar tokens

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.URL_API,
+  baseURL: import.meta.env.VITE_URL_API,
   timeout: 10000,
-  headers: {'Content-Type': 'application/json'}
+  headers: {'Content-Type': 'application/json'},
+  withCredentials: true
 });
 
 export default api;
