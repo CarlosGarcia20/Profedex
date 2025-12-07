259b269babbd14b21a91577092dc6cfc358e2ead
CarlosGarcia20 <cargarcia085@gmail.com>
2025-12-07 11:20:23 -0700

    Actualizacion al socket para que permite enviar token y hacer refresh cuando este se caduque

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,8 +1,33 @@
 import io from 'socket.io-client';
+import api from '../api/axios';
 
 const SOCKET_URL = 'http://localhost:4000'; 
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false,
-    transports: ['websocket'] 
+    transports: ['websocket'],
+    withCredentials: true
+});
+
+socket.on("connect_error", async (err) => {
+    if (err.message.includes("Authentication error") || err.message.includes("jwt expired")) {
+        
+        if (socket.refreshing) return; 
+        socket.refreshing = true;
+
+        console.log("Cookie de socket vencida. Intentando refresh...");
+
+        try {
+            await api.get('/auth/refresh');
+            
+            console.log("Cookie renovada. Reconectando socket...");
+            socket.connect();
+
+        } catch (refreshError) {
+            console.error("Falló el refresh. Sesión expirada.", refreshError);
+            // window.location.href = '/login';
+        } finally {
+            socket.refreshing = false;
+        }
+    }
 });
